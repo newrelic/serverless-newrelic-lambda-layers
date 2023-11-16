3a3cd8e72398b76a0d96479b00cea6e92a22f7f4
mrickard <maurice@mauricerickard.com>
2023-11-16 12:49:19 -0500

    chore: Updated test fixtures
    Signed-off-by: mrickard <maurice@mauricerickard.com>

--- src/index.ts
+++ src/index.ts
@@ -751,7 +751,11 @@ or make sure that you already have Serverless 3.x installed in your project.
   }
 
   private getHandlerWrapper(runtime: string, handler: string) {
-    if (["nodejs14.x", "nodejs16.x", "nodejs18.x", "nodejs20.x"].indexOf(runtime) !== -1) {
+    if (
+      ["nodejs14.x", "nodejs16.x", "nodejs18.x", "nodejs20.x"].indexOf(
+        runtime
+      ) !== -1
+    ) {
       return "newrelic-lambda-wrapper.handler";
     }
 
