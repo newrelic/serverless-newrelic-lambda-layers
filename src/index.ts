518a1c4f4c12e94d67f47936b81029c2e0eb2503
Sashwatdas123 <Sashwatdas@newrelic.com>
2025-12-09 19:34:42 +0530

    feat: fixed lint issues

--- src/index.ts
+++ src/index.ts
@@ -765,9 +765,13 @@ or make sure that you already have Serverless 3.x installed in your project.
 
   private getHandlerWrapper(runtime: string, handler: string) {
     if (
-      ["nodejs16.x", "nodejs18.x", "nodejs20.x", "nodejs22.x", "nodejs24.x"].indexOf(
-        runtime
-      ) !== -1
+      [
+        "nodejs16.x",
+        "nodejs18.x",
+        "nodejs20.x",
+        "nodejs22.x",
+        "nodejs24.x",
+      ].indexOf(runtime) !== -1
     ) {
       return "newrelic-lambda-wrapper.handler";
     }
